279b50782eb3c56228dae559d4eb2f4ca8398aff
May Dotimas <developer@nexbridgetech.com>
2019-07-15 15:56:36 +0800

    update migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email',250)->unique();
+            $table->string('email',100)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
