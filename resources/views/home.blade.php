6284837cb623c18310985ec30029d80b1f2d0130
May Dotimas <dotimasmay@gmail.com>
2019-07-15 23:29:44 +0800

    url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js"></script>
 <script>
-    var socket = io('http://touchpoint-nodejs.nexbridgetech.com');
+    var socket = io('207.38.86.24:13159');
 console.log(socket);
     new Vue({
         el: '#app',
